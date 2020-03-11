7e7f6cbc126853e96ed362cbe73c76de6d5555b4
Hao1229 <s0939859391@gmail.com>
2020-03-11 17:35:31 +0800

    [modify] modify default layout

--- src/components/index/indexContent.vue
+++ src/components/index/indexContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index">
-    <span class="index__text">測試2</span>
+    <span class="index__text">vue core</span>
   </div>
 </template>
 
@@ -12,8 +12,15 @@ export default {
 
 <style lang="scss" scoped>
 .index {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #009FCC;
   &__text {
-    color: red;
+    color: black;
+    font-size: 50px;
   }
 }
 </style>
